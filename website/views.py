06106f6f376a10edaa1f64615bb64c07332f4c37
Preston Danielson <preston.danielson@gmail.com>
2022-04-17 23:48:18 -0500

    Initial commit (repurposing meeting planner tutorial project)

---
+++ website/views.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Hi! My name is Preston Danielson. I am an Electrical and Software Engineer based in Dallas, Texas.")
