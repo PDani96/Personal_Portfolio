907cda4afb9912552f451f3b67a4a6fa680c99c2
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2024-12-30 19:33:26 -0600

    Fixed incorrect static_root and ran collectstatic

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 
 print("base dir path", BASE_DIR)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
