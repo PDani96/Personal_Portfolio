92e82a530a9fb3acd69655c32cb2a2f3f0b7bf53
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2025-01-04 18:41:24 -0600

    Removed chess backend app. Restarting as part of personal_portfolio app.

--- chess_game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chess_game, name='chess_game'),
-    path("chess/", views.chess_frontend, name="chess_app"),
-]
--- chess_game/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def chess_game(request):
-    return render(request, 'chess_game/chess_game.html')
-
-def chess_frontend(request):
-    return render(request, "chess-frontend/index.html")
